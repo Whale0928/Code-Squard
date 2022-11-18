79999c402aeaff5cfdadda54036f70d9da1f8b71
SG_COMPUTER <rlagusrl928@gmail.com>
2022-11-18 17:53:09 +0900

    feat : add Lib Junit5 and create Test Package

---
+++ src/test/BowlingTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class BowlingTest {
+
+    @DisplayName("jUnitTest")
+    @Test
+    void test(){
+        System.out.println("test");
+    }
+}
